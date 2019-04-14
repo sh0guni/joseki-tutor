6c42ca87aa1ecc3c462508db7bdf131b9789d138
Jesse Korpela <donburij@gmail.com>
2019-04-14 10:27:50 +0900

    WIP: Parser

--- __tests__/Board_test.ml
+++ __tests__/Board_test.ml
@@ -1,6 +1,6 @@
 open Jest
 open Sgf
-(* open Belt_MapString *)
+open Belt_MapString
 
 let () =
 
@@ -11,14 +11,75 @@ describe "SGF parsing" (fun() ->
     let expected = None in
     expect (parse "") |>  toEqual expected
   );
+
   test "Tree with no nodes" (fun() ->
     let expected = None in
     expect (parse "()") |> toEqual expected
+  );
+
+  test "Missing closing bracket" (fun() ->
+    let expected = None in
+    expect (parse "(") |> toEqual expected
   );
 
   test "Node without tree" (fun() ->
     let expected = None in
     expect (parse ";") |> toEqual expected
   );
+
+  test "Node without properties" (fun() ->
+    let expected: tree option = Some (Node (empty, [])) in
+    expect (parse "(;)") |> toEqual expected
+  );
+
+  test "Node without properties with missing closing bracket" (fun() ->
+    let expected = None in
+    expect (parse "(;") |> toEqual expected
+  );
+
+  test "Single node tree" (fun() ->
+    let expected: tree option = Some (Node (fromArray [|"A", ["B"]|], [])) in
+    expect (parse "(;A[B])") |> toEqual expected
+  );
+
+  test "Two nodes" (fun() ->
+    let expected: tree option = Some (Node (fromArray [|"A", ["B"]|], [Node (fromArray [|"B", ["C"]|], [])])) in
+    expect (parse "(;A[B];B[C])") |> toEqual expected
+  );
+
+  test "Properties without delimiter" (fun() ->
+    let expected = None in
+    expect (parse "(;A)") |> toEqual expected
+  );
+
+  test "All lowercase property" (fun() ->
+    let expected = None in
+    expect (parse "(;a[b])") |> toEqual expected
+  );
+
+  test "Upper and lowercase property" (fun() ->
+    let expected = None in
+    expect (parse "(;Aa[b])") |> toEqual expected
+  );
+
+  test "Two child trees" (fun() ->
+    let expected = Some (Node (fromArray [|"A", ["B"]|], [Node (fromArray [|"B", ["C"]|], []); Node (fromArray [|"C", ["D"]|], [])])) in
+    expect (parse "(;A[B](;B[C])(;C[D]))") |> toEqual expected
+  );
+
+  test "Multiple property values" (fun() ->
+    let expected = Some (Node (fromArray [|"A", ["b"; "c"; "d"]|], [])) in
+    expect (parse "(;A[b][c][d])") |> toEqual expected
+  );
+
+  test "Escaped property" (fun() ->
+    let expected = Some (Node (fromArray [|"A", ["]b\nc\nd  e \n]"]|], [])) in
+    expect (parse "(;A[\\]b\nc\nd\t\te \n\\]])") |> toEqual expected
+  );
+
+  (*
+  (;FF[4]C[root]SZ[19];B[aa];W[ab])
+
+  *)
 
 );
--- src/sgf/sgf.ml
+++ src/sgf/sgf.ml
@@ -1,1 +1,141 @@
-let parse (_sgf: string) = None
+(* 
+  Collection = GameTree+
+  GameTree   = '(' Sequence GameTree* ')'
+  Sequence   = Node+
+  Node       = ';' Property*
+  Property   = PropIdent PropValue+
+  PropIdent  = UcLetter+
+  PropValue  = '[' CValueType ']'
+  CValueType = (ValueType | Compose)
+  ValueType  = (None | Number | Real | Double | Color | SimpleText | Text | Point  | Move | Stone)
+  UcLetter   = 'A'..'Z'
+  Compose    = ValueType ':' ValueType
+*)
+
+open Belt_MapString
+
+type propertyMap = string list t
+
+type tree = Node of propertyMap * tree list
+
+let explode s =
+  let rec exp i l =
+    if i < 0 then l else exp (i - 1) (s.[i] :: l) in
+  exp (String.length s - 1) []
+
+type token =
+  | LEFT_BRACE
+  | RIGHT_BRACE
+  | LEFT_BRACKET
+  | RIGHT_BRACKET
+  | SEMICOLON
+  | IDENTIFIER of string
+  | INVALID of char
+
+let rec scanIdentifier = function
+  | 'A'..'Z' | 'a'..'z' as c :: xs -> let (id, xs) = scanIdentifier xs in (String.make 1 c ^ id, xs)
+  | xs -> ("", xs)
+
+let rec scan = function 
+  | [] -> []
+  | c :: xs as tokens -> match c with
+    | '(' -> LEFT_BRACE :: scan xs
+    | ')' -> RIGHT_BRACE :: scan xs
+    | ']' -> RIGHT_BRACKET :: scan xs
+    | '[' -> LEFT_BRACKET :: scan xs
+    | ';' -> SEMICOLON :: scan xs
+    | 'A'..'Z' | 'a'..'z' -> let (s, xs) = scanIdentifier tokens in IDENTIFIER(s) :: scan xs
+    | _ -> INVALID c :: []
+
+let debugToken = function
+  | LEFT_BRACE -> "("
+  | RIGHT_BRACE -> ")"
+  | LEFT_BRACKET -> "["
+  | RIGHT_BRACKET -> "]"
+  | SEMICOLON -> ";"
+  | IDENTIFIER x -> "ID " ^ x
+  | INVALID c -> "INVALID " ^ (String.make 1 c)
+
+let debug f x = 
+  Js.log("Debug");
+  Js.log(List.map debugToken x);
+  let x = f @@ x in
+    Js.log x;
+    x
+
+let rec parseValue = function
+  | LEFT_BRACKET :: IDENTIFIER value :: RIGHT_BRACKET :: xs ->
+    (match parseValue xs with
+      | None -> Some([value], xs)
+      | Some (value2, xs) -> Some(value :: value2, xs))
+  | _ -> None
+
+let parseProperty = function
+  | IDENTIFIER name :: xs ->
+    if String.uppercase name == name 
+    then
+      match (debug parseValue) xs with
+        | None -> None
+        | Some (value, xs) -> Some([name, value;], xs)
+    else
+      (Js.log ("Not uppercase" ^ name); None)
+  | _ -> None
+
+let parseProperties tokens =
+  match parseProperty tokens with
+    | None -> None
+    | Some (p, xs) ->
+      (match parseProperty xs with
+        | Some(p2, xs) -> Some (p @ p2, xs)
+        | None -> Some (p, xs))
+
+let parseNode = function
+  | SEMICOLON :: xs -> 
+    (match parseProperty xs with
+      | None -> Some(empty, xs)
+      | Some (props, xs) -> Some (fromArray(Array.of_list props), xs))
+  | _ -> None
+
+let rec parseSequence sequence = match parseNode sequence with
+  | None -> None
+  | Some (prop, xs) -> match parseSequence xs with
+    | Some (seq, xs) -> Some (Node (prop, [seq]), xs)
+    | None -> match parseTree xs with
+      | None -> Some (Node (prop, []), xs)
+      | Some (tree, xs) -> Some (Node (prop, tree), xs)
+
+and parseTree = function
+  | LEFT_BRACE :: xs ->
+      (match parseSequence xs with
+        | Some (sequence, RIGHT_BRACE :: xs) -> 
+          (match parseTree xs with
+           | None -> Some ([sequence], xs)
+           | Some (tree, xs) -> Some(sequence :: tree, xs))
+        | _ -> None)
+  | _ -> None
+
+let parseCollection tokens =
+  match parseTree tokens with
+    | None -> None
+    | x -> x
+
+let tokenize chars =
+  match scan chars with
+  | [] -> None
+  | tokens ->
+    match List.nth tokens (List.length tokens - 1) with
+    | INVALID c ->
+      Js.log(Printf.sprintf "Invalid character %c at position %d" c (List.length tokens));
+      None
+    | _ -> Some tokens
+
+
+let parse (sgf: string): tree option = 
+  match tokenize @@ explode sgf with 
+  | None -> None
+  | Some tokens ->
+    Js.log("Tokens");
+    Js.log(List.map debugToken tokens);
+    match parseTree tokens with
+      | Some(tree, _) -> Some (List.hd tree)
+      | _ -> None
